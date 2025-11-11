cf4891a768fda16ecd2d404531bf7a2b6140adb1
Sergey Gavrielov <sergey.gav82@gmail.com>
2025-11-11 19:58:10 +0200

    commited main_game

--- main_game.py
+++ main_game.py
@@ -13,12 +13,12 @@ list_of_words = [
     "epic"
 ]
 
-# def choose_a_random_word(word_list: list):
-#     len_of_list = len(word_list)
-#     random_index = random.randint(a=0, b=len_of_list - 1)
-#     return word_list[random_index]
-#
-#
+def choose_a_random_word(word_list: list):
+    len_of_list = len(word_list)
+    random_index = random.randint(a=0, b=len_of_list - 1)
+    return word_list[random_index]
+random_word = choose_a_random_word(list_of_words)
+print(random_word)
 # def print_hidden_word(word):
 #     ...
 #
@@ -26,10 +26,18 @@ list_of_words = [
 # if __name__ == '__main__':
 #     print(choose_a_random_word(list_of_words))
 
-def letter(letter):
+def get_letter():
     letter = input("insert letter: ")
     if letter.isalpha() and len(letter) == 1:
         print(letter)
     else:
         print(False)
-letter(letter)
+    return letter
+
+
+inserted_letter = get_letter()
+
+if inserted_letter in random_word:
+    print("letter exists")
+else:
+    print("letter does not exist")
